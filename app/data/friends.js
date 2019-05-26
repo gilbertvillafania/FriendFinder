a21f9110e430e765eebcdfd8af2f125d5201af56
botong719 <botong719@gmail.com>
2019-05-25 21:22:40 -0700

    adding friendfinder files

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Natasha Romanoff",
+		photo: "https://d3c1jucybpy4ua.cloudfront.net/data/62251/big_picture/black-widow.jpg?1554199500",
+		scores: [
+			"5",
+			"1",
+			"3",
+			"1",
+			"5",
+			"2",
+			"5",
+			"4",
+			"1",
+			"1"
+		]
+	},
+	{
+		name: "Shuri",
+		photo: "https://fsmedia.imgix.net/64/06/30/59/c5e6/469c/81cb/ee7db6fc3285/shuri-letita-wright-in-black-panther.jpeg?auto=format%2Ccompress&h=1200&w=1200&crop=edges&fit=crop",
+		scores: [
+			"4",
+			"1",
+			"5",
+			"1",
+			"3",
+			"5",
+			"2",
+			"5",
+			"1",
+			"1"
+		]
+	},
+	{
+		name: "Hela",
+		photo: "https://s2.r29static.com//bin/entry/52e/720x864,85/1880403/image.webp",
+		score: [
+			"1",
+			"5",
+			"1",
+			"5",
+			"3",
+			"1",
+			"3",
+			"1",
+			"5",
+			"5"		
+		]
+	}
+];
+
+module.exports = friends;
